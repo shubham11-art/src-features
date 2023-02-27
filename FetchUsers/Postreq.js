8f9e115b32b49d00563a49c71c385da323255517
Shubham <shubhamg8446@gmail.com>
2023-02-28 00:24:42 +0530

    remove origin

--- FetchUsers/Postreq.js
+++ FetchUsers/Postreq.js
@@ -3,15 +3,14 @@ import React from "react";
 const Postreq = () => {
   return (
     <div>
-      <h1> I am a post request</h1>
-     <table><tr></tr>
-	  </table>
-<h3>  I am rebasing concept   </h3>
-	  <h2>Hello World</h2>
+      <h1> remove origin</h1>
+      <table>
+        <tr></tr>
+      </table>
+      <h3> I am rebasing concept </h3>
+      <h2>Hello World</h2>
     </div>
   );
 };
 
 export default Postreq;
-
-
