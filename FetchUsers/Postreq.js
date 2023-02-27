16e8f37ddc06869fe868e80961091ebb4762ab4f
Shubham <shubhamg8446@gmail.com>
2023-02-28 00:43:15 +0530

    error free commit

--- FetchUsers/Postreq.js
+++ FetchUsers/Postreq.js
@@ -3,12 +3,12 @@ import React from "react";
 const Postreq = () => {
   return (
     <div>
-      <h1> remove origin</h1>
+      <h1> I am Shubhamg</h1>
       <table>
         <tr></tr>
       </table>
       <h3> I am rebasing concept </h3>
-      <h2>Hello World</h2>
+      <h2>No One</h2>
     </div>
   );
 };
